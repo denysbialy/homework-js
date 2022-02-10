3d4fe4f1510520d1c14793c918863d82759ec337
denysbialy <dnsbls11@gmail.com>
2022-02-11 00:33:15 +0200

    Add first realese forEach

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 function MyArray() {
-  this.length = 0;
+  // this.length = 0;
+  
+  // this.push = function (value) {
+  //   this[this.length] = value;
 
-  this.push = function (value) {
-    this[this.length] = value;
+  //   this.length++;
+  //   return this.length;
+  // };
 
-    this.length++;
-    return this.length;
-  };
+  // this.pop = function () {
+  //   if (this.length !== 0) {
+  //     this.length--;
+  //   }
+  //   let deleteValue = this[this.length];
+  //   delete this[this.length];
+  //   return deleteValue;
+  // };
 
-  this.pop = function () {
-    if (this.length !== 0) {
-      this.length--;
+  this.forEach = function (callback) {
+    debugger
+    for (let i = 0; i < this.length; i++) {
+      this[i] = callback;
+      // console.log(this[callback[i]]);
+      return callback;
     }
-    let deletValue = this[this.length];
-    delete this[this.length];
-    return deletValue;
   };
+  
 }
 
+// const array1 = ['a', 'b', 'c'];
+
+// array1.forEach(element => console.log(element));
+
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
+
 const newMyArr = new MyArray();
+
