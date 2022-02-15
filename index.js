2bf8053a28ed9e1e7e127e575fe3c919d0e5362d
Denys Bilousenko <dnsbls11@gmail.com>
2022-02-15 20:50:50 +0200

    Add getter setter

--- index.js
+++ index.js
@@ -5,9 +5,114 @@ class User {
     this.age = age;
   }
 
-  getFullName = () => `${this.name} ${this.surname}`;
-  isAdult = () => this.age >= 18;
+  getFullName(){
+    `${this.name} ${this.surname}`;
+  }
+  isAdult(){
+     this.age >= 18;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    if (typeof newName !== "string") {
+      throw TypeError("Type is not string");
+    }
+    if (newName.trim() === "") {
+      throw RangeError("name is not defined");
+    }
+    this._name = newName;
+  }
+  get surname() {
+    return this._surname;
+  }
+  set surname(newSurname) {
+    if (typeof newSurname !== "string") {
+      throw TypeError("Type is not string");
+    }
+    if (newSurname.trim() === "") {
+      throw RangeError("surname is not defined");
+    }
+    this._surname = newSurname;
+  }
+
+  get age() {
+    return this._age;
+  }
+  set age(newAge) {
+    if (isNaN(newAge)) {
+      throw TypeError("age is not number");
+    }
+    if (newAge > 120 || newAge < 0) {
+      throw RangeError("Invalid age, create correct age");
+    }
+    this._age = newAge;
+  }
 }
 
-const user1 = new User("Viktor", "Rasputin", 44);
-const user2 = new User("Wiktor", "Diperov", 12);
+class Moder {
+  constructor(name, surname, age, email) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.email = email;
+  }
+  getFullName = () => `${this.name} ${this.surname}`;
+  isAdult = () => this.age >= 18;
+  createMessage = (message) => `Создать сообщение: ${message} `;
+  deleteMessage = () => "Удалить сообщение";
+
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    if (typeof newName !== "string") {
+      throw TypeError("Type is not string");
+    }
+    if (newName.trim() === "") {
+      throw RangeError("name is not defined");
+    }
+    this._name = newName;
+  }
+  get surname() {
+    return this._surname;
+  }
+  set surname(newSurname) {
+    if (typeof newSurname !== "string") {
+      throw TypeError("Type is not string");
+    }
+    if (newSurname.trim() === "") {
+      throw RangeError("surname is not defined");
+    }
+    this._surname = newSurname;
+  }
+
+  get age() {
+    return this._age;
+  }
+  set age(newAge) {
+    if (isNaN(newAge)) {
+      throw TypeError("age is not number");
+    }
+    if (newAge > 120 || newAge < 0) {
+      throw RangeError("Invalid age, create correct age");
+    }
+    this._age = newAge;
+  }
+
+  get email() {
+    return this._email;
+  }
+  set email(newEmail) {
+    if (typeof newEmail !== "string") {
+      throw TypeError("Type is not email");
+    }
+    if (newEmail === "") {
+      throw RangeError("email is not defined");
+    }
+    this._email = newEmail;
+  }
+}
+
+const moder = new Moder("Ivan", "Ivanov", 12, "test@test.test");
